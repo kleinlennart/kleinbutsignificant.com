44a245e6a30426b5b5c786ff0f9105ce99547938
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2024-01-18 00:37:57 +0000

    add python support

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -9,8 +9,19 @@ git submodule init && git submodule update
 
 
 # https://github.com/rocker-org/devcontainer-features/tree/main/src/quarto-cli#execution-engine
-# "postCreateCommand": "python3 -m pip install jupyter"
+python3 -m pip install --upgrade pip
 
+## Essential for quarto
+python3 -m pip install jupyter
+python3 -m pip install jupyter-cache
+# quarto check jupyter
 
+## Install all other env packages
+python3 -m pip install -r requirements.txt
+# python3 -m pip freeze > requirements.txt
+
+# https://docs.github.com/en/codespaces/setting-up-your-project-for-codespaces/adding-a-dev-container-configuration/setting-up-your-python-project-for-codespaces
+# // Use 'postCreateCommand' to run commands after the container is created.
+# "postCreateCommand": "pip3 install --user -r requirements.txt",
 
 # "postCreateCommand": "echo 'options(repos = c(CRAN = \"https://cloud.r-project.org\"))' | sudo sh -c 'cat - >>\"${R_HOME}/etc/Rprofile.site\"'",
---
+++ .devcontainer/setup.py
@@ -0,0 +1,23 @@
+# https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/#creating-a-virtual-environment
+
+## Check python
+# python3 --version
+
+## Check jupyter
+
+## Prepare pip
+# python3 -m pip install --upgrade pip
+# python3 -m pip --version
+
+## Install new packages or upgrade
+# python3 -m pip install numpy
+# python3 -m pip install --upgrade requests
+
+## Export all installed packages
+# python3 -m pip freeze > requirements.txt
+
+## Upgrade all installed packages
+# pip install -r requirements.txt --upgrade 
+
+## Install env packages (not exclusive)
+# python3 -m pip install -r requirements.txt
